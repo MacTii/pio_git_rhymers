1f46bd2158a2d9a7f439f085ee39a03f4c09ad30
MacTii <mactii41@gmail.com>
2022-01-26 20:57:35 +0100

    1.4 accesory 4 delete unnecessary getters and setters

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -44,12 +44,4 @@ public class IntLinkedList {
     public void setLast(Node last) {
         this.last = last;
     }
-
-    public int getI() {
-        return i;
-    }
-
-    public void setI(int i) {
-        this.i = i;
-    }
 }
