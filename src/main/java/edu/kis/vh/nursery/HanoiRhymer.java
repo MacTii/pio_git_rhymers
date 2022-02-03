4870bc3aaa6eaceae55fe808056b317b01d1891d
MacTii <mactii41@gmail.com>
2022-02-03 19:39:11 +0100

    1.5.6 generate documentation skeleton

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,12 +2,22 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
+    /**
+     * counting out class
+     */
     private int totalRejected = 0;
 
+    /**
+     * return total rejected int
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * counting total rejected
+     * @param in input number to count in
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > getLastNumber())
